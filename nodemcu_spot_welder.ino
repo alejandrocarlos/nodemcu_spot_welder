d2ea2b0a1b4f991f1af4d0356f6baa558c95f97a
Alejandro Carlos <axel.larcos@gmail.com>
2023-01-25 17:31:23 -0800

    Converted code to be compatible with arduino uno

--- nodemcu_spot_welder.ino
+++ nodemcu_spot_welder.ino
@@ -2,26 +2,24 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "AiEsp32RotaryEncoder.h"
+#include <Encoder.h>
 #include <Fonts/FreeSans9pt7b.h>
 
-#define ROTARY_ENCODER_A_PIN 14
-#define ROTARY_ENCODER_B_PIN 12
-#define ROTARY_ENCODER_BUTTON_PIN 13
-#define ROTARY_ENCODER_VCC_PIN -1
+#define ROTARY_ENCODER_A_PIN 2
+#define ROTARY_ENCODER_B_PIN 3
 #define ROTARY_ENCODER_STEPS 5
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-#define TRIGGER_BUTTON_PIN 13
-#define RELAY_PIN 15
+#define TRIGGER_BUTTON_PIN 4
+#define RELAY_PIN 5
 
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+Encoder rotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN);
 
 long prevMilliseconds = 0;
 long relayMilliseconds = 0;
@@ -30,11 +28,6 @@ bool prevRelayOn = false;
 bool relayOn = false;
 int buttonCurrent = HIGH;
 int buttonPrev = HIGH;
-
-void IRAM_ATTR readEncoderISR()
-{
-	rotaryEncoder.readEncoder_ISR();
-}
 
 void printEncoderReading() {
   display.clearDisplay();
@@ -66,10 +59,6 @@ void setup() {
   display.setTextColor(WHITE);
 
   bool circleValues = false;
-  rotaryEncoder.begin();
-	rotaryEncoder.setup(readEncoderISR);
-  rotaryEncoder.setBoundaries(0, 500, circleValues); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
-
   pinMode(TRIGGER_BUTTON_PIN, INPUT_PULLUP);
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
@@ -106,7 +95,15 @@ void triggerRelay() {
 
 void loop() {
   // rotary_loop();
-  relayMilliseconds = rotaryEncoder.readEncoder() * 10;
+  long reading = rotaryEncoder.read();
+
+  if (reading < 0){
+    relayMilliseconds = 0;
+    rotaryEncoder.write(0);
+  } else {
+    relayMilliseconds = (reading / 4) * 10;
+  }
+
   loopCount = loopCount + 1;
 
   checkButton();
